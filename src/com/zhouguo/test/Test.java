74148cb0159435668c5d2111ba5c0856f9e725a1
zhouguo_abc <zhouguo_abc@163.com>
2016-04-29 20:36:58 +0800

    update 04

--- src/com/zhouguo/test/Test.java
+++ src/com/zhouguo/test/Test.java
@@ -4,5 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("第一条语句！！！");
 		System.out.println("第二条语句！！！");
+		System.out.println("第三条语句！！！");
+		System.out.println("第四条语句！！！");
 	}
 }
