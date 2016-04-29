2cc0ba03e3f8044692cedbaeaf554934515c644d
zhouguo_abc <zhouguo_abc@163.com>
2016-04-29 20:43:59 +0800

    maopao 00

--- src/com/zhouguo/test/Test.java
+++ src/com/zhouguo/test/Test.java
@@ -2,9 +2,19 @@ package com.zhouguo.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("第一条语句！！！");
-		System.out.println("第二条语句！！！");
-		System.out.println("第三条语句！！！");
-		System.out.println("第四条语句！！！");
+		int[] arr = new int[]{9,4,10,6,2,8,13};
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if(arr[j] > arr[j + 1]){
+					int tmp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = tmp;
+				}
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 }
